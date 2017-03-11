2aa1c34fc031ad82e3edc2dd45350afca923d964
Robert Brice <rob.a.brice@gmail.comC>
2017-03-11 09:32:34 +0000

    cleaning up and commenting CardValue

--- card_game/CardValue.java
+++ card_game/CardValue.java
@@ -1,10 +1,9 @@
 package card_game;
 
-// there's a whole lot of special magic behind the scenes with enums...
+// there's a whole lot of magic going on behind the scenes with enums...the least of which is that the compiler assigns the constants in brackets to the constructor argument and so we can associate any values we want with our constant. Enums are a datatype and should aways be used to contain constants which will never change in our program. 
 
 public enum CardValue {
 
-  ACE(1, 111),
   TWO(2, 112),
   THREE(3, 113),
   FOUR(4, 114),
@@ -16,7 +15,8 @@ public enum CardValue {
   TEN(10, 1110),
   JACK(11, 1111),
   QUEEN(12, 1112),
-  KING(13, 1113);
+  KING(13, 1113),
+  ACE(14, 1114);
 
   private int numberValue1;
   private int numberValue2;
@@ -36,11 +36,10 @@ public enum CardValue {
 
   public String toString() {
     switch(this) {
-      case ACE:       return "ace";
       case TWO:       return "two";
       case THREE:     return "three";
       case FOUR:      return "four";
-      case FIVE:       return "five";
+      case FIVE:      return "five";
       case SIX:       return "six";
       case SEVEN:     return "seven";
       case EIGHT:     return "eight";
@@ -49,6 +48,7 @@ public enum CardValue {
       case JACK:      return "jack";
       case QUEEN:     return "queen";
       case KING:      return "king";
+      case ACE:       return "ace";
       default:        return "unspecified";
     }
 
